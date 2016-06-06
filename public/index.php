0f105d830980075e2b4ffa19ed84fb1a5172a3c6
xianhuawei <258022429@qq.com>
2016-06-06 17:51:17 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,12 +10,14 @@
 define('URL_BASE', strtr($_SERVER['SCRIPT_NAME'], array("\\" => '/', 'index.php' => '')));
 # 网站入口 URL，适配是否开启伪静态
 define('URL_ENTRANCE',rtrim(URL_BASE . 'index.php','/'));
-# 网站入口所在的根目录，适配上传、生成路径
+# 网站入口所在的根目录，适配生成路径
 define('PATH_BASE',dirname(__FILE__));
 # 网站应用所有的目录，适配配置文件
 define('PATH_APP',dirname(dirname(__FILE__)));
 # 目录分割符
 define("DS", '/');
+define('UPLOAD_PATH',PATH_BASE + '../uploads');
+define('LOGS_PATH',PATH_BASE + '../logs');
 
 if (!extension_loaded('yaf')) {
     #we should load the framework from classes is the extension is not loaded
